a0a7154e71e040b76e60318621171d5a7ee4fb5e
crizzo16 <crizzo16@users.noreply.github.com>
2017-03-28 13:53:26 -0700

    Create consent.client.controller.js

---
+++ modules/core/client/controllers/consent.client.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('core')
+    .controller('ConsentController', ConsentController);
+
+  function ConsentController() {
+    var vm = this;
+    
+
+  
+  }
+}
